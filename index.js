ec8c3fe44281e07b5dde380704e22eb0323fa080
Jack Lu <jacklu@jacklu.me>
2017-11-15 23:56:49 +0800

    js2html->css-select-adapter-estree

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 var acorn = require('acorn');
 var escodegen = require('escodegen');
 var cssx = require('cssx');
-var js2html = require('js2html');
+var select = require('css-select');
+var adapter = require('css-select-adapter-estree').adapter;
+var Node = require('css-select-adapter-estree').Node;
 function transform(_cssx) {
 	return function (js) {
 		var js = acorn.parse(js);
-		var html = js2html.js2html(js);
-		var $ = html.constructor;
-		var root = $.root();
-		root.append(html);
+		var root = {
+			find: function (selector) {
+				return select(selector, new Node(js), { adapter: adapter });
+			}
+		};
 		_cssx = cssx.parse(_cssx);
 		var elementrule = cssx(_cssx)(root);
 		for (var id in elementrule)
-			apply(elementrule[id].style, $(elementrule[id].element));
-		var js = js2html.html2js(html);
+			apply(elementrule[id].style, elementrule[id].element);
 		return escodegen.generate(js);
 	}
 }
@@ -21,15 +23,13 @@ function apply(style, element) {
 	for (var name in style)
 		switch (name) {
 			case 'value':
-				element.text(style[name]);
+				element.value.value = style[name];
 				break;
 			case 'before':
-				var $ = element.constructor;
 				var js = style[name];
 				var program = acorn.parse(js);
 				var js = program.body[0];
-				var html = js2html.js2html(js);
-				element.before(html);
+				element.before(js);
 				break;
 		}
 }
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var transform = require('.');
 var js = "(function(){var a=0;return a;})();";
-var cssx = 'literal{"value":123}identifier{"value":"b"}return{"before":"console.log(\'return\');"}';
+var cssx = '.literal{"value":123}.return{"before":"console.log(\'return\');"}';
 console.log(js);
 var js = transform(cssx)(js);
 console.log(js);
